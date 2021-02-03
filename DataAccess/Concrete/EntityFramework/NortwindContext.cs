896afe5cce92574294b20d16cff0d8d4a90b64d2
Yavuz <Yavuz@YAVUZKRAL>
2021-02-03 23:47:51 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NortwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\msqllocaldb;Database=Nortwind,Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CatagoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=12, },
+                new Product{ProductId=2, CatagoryId=1, ProductName="Kamera", UnitPrice=15, UnitsInStock=12, },
+                new Product{ProductId=3, CatagoryId=2, ProductName="Telefon" ,UnitPrice=15, UnitsInStock=12, },
+                new Product{ProductId=4, CatagoryId=2, ProductName="Klavye", UnitPrice=15, UnitsInStock=12, },
+                new Product{ProductId=5, CatagoryId=2, ProductName="Fare", UnitPrice=15, UnitsInStock=12, }
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+           
+            Product productToDelete  = _products.SingleOrDefault(p=> p.ProductId == product.ProductId); 
+
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CatagoryId = product.CatagoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public List<Product> GetAllByCatagory(int catagoryId)
+        {
+           return  _products.Where(p => p.CatagoryId == catagoryId).ToList();
+
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
